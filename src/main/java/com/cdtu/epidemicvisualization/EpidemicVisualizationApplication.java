f898d8de8124a8a970a15a7dc4f423f1ade3d7d5
charon <960877165@qq.com>
2022-06-18 13:55:24 +0800

    Initial commit

---
+++ src/main/java/com/cdtu/epidemicvisualization/EpidemicVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.cdtu.epidemicvisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicVisualizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdtu/epidemicvisualization/EpidemicVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdtu.epidemicvisualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicVisualizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
